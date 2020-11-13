7c6e8114b810276d4b3fc12259c6087746e1b6a2
rigatoni004 <71712533+rigatoni004@users.noreply.github.com>
2020-11-13 14:16:21 -0500

    Update FinalProject.java

--- FinalProject.java
+++ FinalProject.java
@@ -1,6 +1,6 @@
 /*
  * Programmer: Riley Wilson
- * Date: 11/6/2020
+ * Date: 11/13/2020
  * Purpose: To run a card game of blackjack
  */
 // Import the GUI libraries
@@ -14,7 +14,7 @@ public class FinalProject {
 	/**
 	 * MAIN METHOD
 	 * This main method starts the GUI and runs the createMainWindow() method.
-     * This method should not be changed.
+	 * This method should not be changed.
 	 */
 	public static void main (String [] args) {
 		javax.swing.SwingUtilities.invokeLater (new Runnable () {
@@ -28,161 +28,163 @@ public class FinalProject {
 	/**
 	 * STATIC VARIABLES AND CONSTANTS
 	 * Declare the objects and variables that you want to access across
-     * multiple methods.
+	 * multiple methods.
 	 */
 	static JLabel userScoreNumber;
-	
+
 	static JPanel cardPanel2;
-	
+
 	static JPanel cardPanel;
-	
+
 	static JLabel computerScoreNumber;
-	
+
 	static int [] cardValue = {
-		1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4, 
-		5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,
-		9,9,9,9,10,10,10,10,10,10,10,10,
-		10,10,10,10,10,10,10,10}
-	    ;
-	
-	static int randomCard;
-	
+			1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4, 
+			5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,
+			9,9,9,9,10,10,10,10,10,10,10,10,
+			10,10,10,10,10,10,10,10}
+	;
+
+	static int nextCard = 0;
+
 	static JLabel [] cards = {
 			createScaledImage("AC.png",100, 150),
 			createScaledImage("AD.png",100, 150),
 			createScaledImage("AH.png",100, 150),
 			createScaledImage("AS.png",100, 150),
-			
+
 			createScaledImage("2C.png",100, 150),
 			createScaledImage("2D.png",100, 150),
 			createScaledImage("2H.png",100, 150),
 			createScaledImage("2S.png",100, 150),
-			
+
 			createScaledImage("3C.png",100, 150),
 			createScaledImage("3D.png",100, 150),
 			createScaledImage("3H.png",100, 150),
 			createScaledImage("3S.png",100, 150),
-			
+
 			createScaledImage("4C.png",100, 150),
 			createScaledImage("4D.png",100, 150),
 			createScaledImage("4H.png",100, 150),
 			createScaledImage("4S.png",100, 150),
-			
+
 			createScaledImage("5C.png",100, 150),
 			createScaledImage("5D.png",100, 150),
 			createScaledImage("5H.png",100, 150),
 			createScaledImage("5S.png",100, 150),
-			
+
 			createScaledImage("6C.png",100, 150),
 			createScaledImage("6D.png",100, 150),
 			createScaledImage("6H.png",100, 150),
 			createScaledImage("6S.png",100, 150),
-			
+
 			createScaledImage("7C.png",100, 150),
 			createScaledImage("7D.png",100, 150),
 			createScaledImage("7H.png",100, 150),
 			createScaledImage("7S.png",100, 150),
-			
+
 			createScaledImage("8C.png",100, 150),
 			createScaledImage("8D.png",100, 150),
 			createScaledImage("8H.png",100, 150),
 			createScaledImage("8S.png",100, 150),
-			
+
 			createScaledImage("9C.png",100, 150),
 			createScaledImage("9D.png",100, 150),
 			createScaledImage("9H.png",100, 150),
 			createScaledImage("9S.png",100, 150),
-			
+
 			createScaledImage("10C.png",100, 150),
 			createScaledImage("10D.png",100, 150),
 			createScaledImage("10H.png",100, 150),
 			createScaledImage("10S.png",100, 150),
-			
+
 			createScaledImage("JC.png",100, 150),
 			createScaledImage("JD.png",100, 150),
 			createScaledImage("JH.png",100, 150),
 			createScaledImage("JS.png",100, 150),
-			
+
 			createScaledImage("QC.png",100, 150),
 			createScaledImage("QD.png",100, 150),
 			createScaledImage("QH.png",100, 150),
 			createScaledImage("QS.png",100, 150),
-			
+
 			createScaledImage("KC.png",100, 150),
 			createScaledImage("KD.png",100, 150),
 			createScaledImage("KH.png",100, 150),
 			createScaledImage("KS.png",100, 150),
 	};
-	
+
+	static JFrame frame;
+
 
 	/**
 	 * CREATE MAIN WINDOW
-     * This method is called by the main method to set up the main GUI window.
+	 * This method is called by the main method to set up the main GUI window.
 	 */
 	private static void createMainWindow () {
 		// Create and set up the window.
-		JFrame frame = new JFrame ("Blackjack");
+		frame = new JFrame ("Blackjack");
 		frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 		frame.setResizable (false);
-		
+
 		// The panel that will hold the components in the frame.
 		JPanel contentPane = new JPanel ();
 		contentPane.setPreferredSize(new Dimension(900, 400));
-		
+
 		// Make Layout
 		contentPane.setLayout(new BorderLayout());
-		
+
 		// Make right panel
 		JPanel rightPanel = new JPanel();
 		rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.PAGE_AXIS));
 		rightPanel.setPreferredSize(new Dimension(190, 300));
 		rightPanel.setBorder(new EmptyBorder(20, 20, 20, 20));
 		contentPane.add(rightPanel, BorderLayout.EAST);
-		
+
 		//Create game title
 		JLabel gameTitle = new JLabel("Blackjack");
 		rightPanel.add(gameTitle);
 		gameTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
-		
+
 		//Add Filler
 		rightPanel.add(Box.createVerticalGlue());
-		
+
 		//Make new game button buttons
 		JButton newGameButton = new JButton("New Game");
 		newGameButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 		newGameButton.addActionListener(new NewGameHandler());
 		rightPanel.add(newGameButton);
 		rightPanel.add(Box.createRigidArea(new Dimension(135, 10)));
-		
+
 		//Make rules button
 		JButton rulesButton = new JButton("Rules");
 		rulesButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 		rightPanel.add(rulesButton);
 		rightPanel.add(Box.createRigidArea(new Dimension(135, 10)));
-		
+
 		//Make quit button
 		JButton quitButton = new JButton("Quit");
 		quitButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 		quitButton.addActionListener(new QuitButtonListener());
 		rightPanel.add(quitButton);
 		rightPanel.add(Box.createRigidArea(new Dimension(135, 10)));
-		
+
 		//Make table panel
 		JLayeredPane tablePanel = new JLayeredPane();
 		contentPane.add(tablePanel, BorderLayout.CENTER);
-		
+
 		//Add table background
 		JLabel tableImage = new JLabel(new ImageIcon("green table.jfif"));
 		tablePanel.add(tableImage, Integer.valueOf(-300));
 		tableImage.setSize(700, 400);
 		tableImage.setLocation(0, 0);
-		
+
 		//Add deck of cards
 		JLabel deck = createScaledImage("red_back.png", 100, 150);
 		deck.setSize(100, 150);
 		deck.setLocation(25, 150);
 		tablePanel.add(deck, Integer.valueOf(100));
-		
+
 		//Create user card panel
 		cardPanel = new JPanel();
 		tablePanel.add(cardPanel, Integer.valueOf(501));
@@ -190,13 +192,12 @@ public class FinalProject {
 		cardPanel.setLocation(175, 200);
 		cardPanel.setOpaque(false);
 		cardPanel.setLayout(new BoxLayout(cardPanel, BoxLayout.LINE_AXIS));
-		
+
 		//Create random cards(user)
-		randomCardHandler(cards);
-		cardPanel.add(cards[randomCard]);
-		randomCardHandler(cards); 
-		cardPanel.add(cards[randomCard]);
-		
+		shuffle();
+		dealCard();
+		dealCard();
+
 		//Create computer card panel
 		cardPanel2 = new JPanel();
 		tablePanel.add(cardPanel2, Integer.valueOf(501));
@@ -204,11 +205,10 @@ public class FinalProject {
 		cardPanel2.setLocation(175, 0);
 		cardPanel2.setOpaque(false);
 		cardPanel2.setLayout(new BoxLayout(cardPanel2, BoxLayout.LINE_AXIS));
-		
+
 		//Add computer cards
-		cardPanel2.add(deck); 
-		cardPanel2.add(deck);
-		
+
+
 		/*//Add user score
 		JLabel userScore = new JLabel("Score:");
 		userScore.setSize(150, 75);
@@ -216,8 +216,8 @@ public class FinalProject {
 		userScore.setForeground(Color.WHITE);
 		tablePanel.add(userScore);
 		tablePanel.setLayer(userScore, 200);
-		
-		
+
+
 		//add user score number
 		userScoreNumber = new JLabel("0");
 		tablePanel.add(userScoreNumber);
@@ -225,7 +225,7 @@ public class FinalProject {
 		userScoreNumber.setLocation(645, 350);
 		userScoreNumber.setForeground(Color.WHITE);
 		tablePanel.setLayer(userScoreNumber, 200);
-		
+
 		//Add computer score
 		JLabel computerScore = new JLabel("Score:");
 		tablePanel.add(computerScore);
@@ -233,7 +233,7 @@ public class FinalProject {
 		computerScore.setLocation(600, 0);
 		computerScore.setForeground(Color.WHITE);
 		tablePanel.setLayer(computerScore, 201);
-		
+
 		//Add computer score number
 		computerScoreNumber = new JLabel("0");
 		tablePanel.add(computerScoreNumber);
@@ -241,8 +241,8 @@ public class FinalProject {
 		computerScoreNumber.setLocation(645, 0);
 		computerScoreNumber.setForeground(Color.WHITE);
 		tablePanel.setLayer(computerScoreNumber, 201);
-		*/
-		
+		 */
+
 		//Create button Panel
 		JPanel buttonPanel = new JPanel();
 		tablePanel.add(buttonPanel, Integer.valueOf(500));
@@ -251,7 +251,7 @@ public class FinalProject {
 		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.LINE_AXIS));
 		tablePanel.setLayer(buttonPanel, 405);
 		buttonPanel.setOpaque(false);
-		
+
 		//Add card button
 		JButton addCard = new JButton("Hit");
 		addCard.setPreferredSize(new Dimension(50, 25));
@@ -259,20 +259,21 @@ public class FinalProject {
 		addCard.setLocation(100, 0);
 		buttonPanel.add(addCard);
 		buttonPanel.add(Box.createRigidArea(new Dimension(5,0)));
-		
+
 		//Add no card button
 		JButton noCard = new JButton("Stand");
 		noCard.setPreferredSize(new Dimension(50, 25));
+		noCard.addActionListener(new StayButtonHandler());
 		addCard.setLocation(100, 25); 
 		buttonPanel.add(noCard);
 		buttonPanel.add(Box.createRigidArea(new Dimension(5,0)));
-	
-		
-		
+
+
+
 		// Add the panel to the frame
 		frame.setContentPane(contentPane);
-		
-		
+
+
 		//size the window.
 		frame.pack();
 		frame.setLocationRelativeTo(null);
@@ -280,57 +281,73 @@ public class FinalProject {
 	}
 
 
-    /**
-     * HELPER METHODS
-     * Methods that you create to manage repetitive tasks.
-     */
+	/**
+	 * HELPER METHODS
+	 * Methods that you create to manage repetitive tasks.
+	 */
 	private static JLabel createScaledImage (String filename, int width, int height) {
 		Image originalImage = new ImageIcon(filename).getImage();
 		Image scaledImage = originalImage.getScaledInstance(width, height, Image.SCALE_SMOOTH);
 		return new JLabel(new ImageIcon(scaledImage));
 	}
-	
-	private static void randomCardHandler(JLabel [] cards) {
-		Random randomGenerator = new Random();
-		randomCard = randomGenerator.nextInt(53); 
-		cards [randomCard].setVisible(true);
-		cards [randomCard].setSize(80, 130);
-		
+
+	private static void shuffle () {
+		Random rand = new Random();
+		for (int index = 0; index < cards.length; index++) {
+			int randomIndexToSwap = rand.nextInt(cards.length);
+			JLabel temp = cards[randomIndexToSwap];
+			cards[randomIndexToSwap] = cards[index];
+			cards[index] = temp;
+			int tempValue = cardValue[randomIndexToSwap];
+			cardValue[randomIndexToSwap] = cardValue[index];
+			cardValue[index] = tempValue;
+		}
+		nextCard = 0;
 	}
 
-    /**
-     * EVENT LISTENERS
-     * Subclasses that handle events (button clicks, mouse clicks and moves,
-     * key presses, timer expirations)
-     */
-	
-	
+	/**Generates and shuffles random cards*/
+	private static void dealCard() {
+		cardPanel.add(cards[nextCard]);
+		nextCard++;
+	}
+
+	/**
+	 * EVENT LISTENERS
+	 * Subclasses that handle events (button clicks, mouse clicks and moves,
+	 * key presses, timer expirations)
+	 */
+
+
 	/** Handles clicks on add card button */
 	private static class HitButtonHandler implements ActionListener {
 		public void actionPerformed (ActionEvent event) {
-			randomCardHandler(cards); 
-			cardPanel.add(cards[randomCard]);
-			cards[randomCard].setVisible(true);
-			
+			dealCard();
 		}
 	}
-	
+	private static class StayButtonHandler implements ActionListener {
+		public void actionPerformed (ActionEvent event) {
+
+
+		}
+	}
+
 	/**Handles clicks on new game button */
-    private static class NewGameHandler implements ActionListener {
-        public void actionPerformed (ActionEvent event) {
-            
-        	
-        }
-    }
-	
-	   /** Handles clicks on quit button */
-    private static class QuitButtonListener implements ActionListener {
-        public void actionPerformed (ActionEvent event) {
-        	int answer = JOptionPane.showConfirmDialog(null, "Are you sure you want to quit?", 
-        			"Quit?", JOptionPane.YES_NO_OPTION);
-        	if (answer == JOptionPane.YES_OPTION) {
-            System.exit(0);
-        	}
-        }
-    }
+	private static class NewGameHandler implements ActionListener {
+		public void actionPerformed (ActionEvent event) {
+			shuffle();
+
+		}
+	}
+
+
+	/** Handles clicks on quit button */
+	private static class QuitButtonListener implements ActionListener {
+		public void actionPerformed (ActionEvent event) {
+			int answer = JOptionPane.showConfirmDialog(null, "Are you sure you want to quit?", 
+					"Quit?", JOptionPane.YES_NO_OPTION);
+			if (answer == JOptionPane.YES_OPTION) {
+				System.exit(0);
+			}
+		}
+	}
 }
