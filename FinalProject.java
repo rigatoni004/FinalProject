a37e653efb30b7eaedd47fdebb606fee22baa431
rigatoni004 <71712533+rigatoni004@users.noreply.github.com>
2020-11-13 08:47:10 -0500

    Commit to add changes to repl

--- FinalProject.java
+++ FinalProject.java
@@ -1,6 +1,6 @@
 /*
  * Programmer: Riley Wilson
- * Date: 11/5/2020
+ * Date: 11/6/2020
  * Purpose: To run a card game of blackjack
  */
 // Import the GUI libraries
@@ -30,6 +30,10 @@ public class FinalProject {
 	 * Declare the objects and variables that you want to access across
      * multiple methods.
 	 */
+	static JLabel userScoreNumber;
+	
+	static JLabel computerScoreNumber;
+	
 	static JLabel [] cards = {
 			createScaledImage("AC.png",100, 150),
 			createScaledImage("AD.png",100, 150),
@@ -96,7 +100,7 @@ public class FinalProject {
 			createScaledImage("KH.png",100, 150),
 			createScaledImage("KS.png",100, 150),
 	};
-
+	
 
 	/**
 	 * CREATE MAIN WINDOW
@@ -107,16 +111,13 @@ public class FinalProject {
 		JFrame frame = new JFrame ("Blackjack");
 		frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 		frame.setResizable (false);
-
 		
 		// The panel that will hold the components in the frame.
 		JPanel contentPane = new JPanel ();
 		contentPane.setPreferredSize(new Dimension(900, 400));
 		
-		
 		// Make Layout
 		contentPane.setLayout(new BorderLayout());
-		
 		
 		// Make right panel
 		JPanel rightPanel = new JPanel();
@@ -125,30 +126,26 @@ public class FinalProject {
 		rightPanel.setBorder(new EmptyBorder(20, 20, 20, 20));
 		contentPane.add(rightPanel, BorderLayout.EAST);
 		
-		
 		//Create game title
 		JLabel gameTitle = new JLabel("Blackjack");
 		rightPanel.add(gameTitle);
 		gameTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
 		
-	
 		//Add Filler
 		rightPanel.add(Box.createVerticalGlue());
-		
 		
 		//Make new game button buttons
 		JButton newGameButton = new JButton("New Game");
 		newGameButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		newGameButton.addActionListener(new NewGameHandler());
 		rightPanel.add(newGameButton);
 		rightPanel.add(Box.createRigidArea(new Dimension(135, 10)));
-		
 		
 		//Make rules button
 		JButton rulesButton = new JButton("Rules");
 		rulesButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 		rightPanel.add(rulesButton);
 		rightPanel.add(Box.createRigidArea(new Dimension(135, 10)));
-		
 		
 		//Make quit button
 		JButton quitButton = new JButton("Quit");
@@ -157,11 +154,9 @@ public class FinalProject {
 		rightPanel.add(quitButton);
 		rightPanel.add(Box.createRigidArea(new Dimension(135, 10)));
 		
-		
 		//Make table panel
 		JLayeredPane tablePanel = new JLayeredPane();
 		contentPane.add(tablePanel, BorderLayout.CENTER);
-		
 		
 		//Add table background
 		JLabel tableImage = new JLabel(new ImageIcon("green table.jfif"));
@@ -169,13 +164,82 @@ public class FinalProject {
 		tableImage.setSize(700, 400);
 		tableImage.setLocation(0, 0);
 		
-		
 		//Add deck of cards
 		JLabel deck = createScaledImage("red_back.png", 80, 130);
 		deck.setSize(80, 130);
 		deck.setLocation(25, 150);
-		
 		tablePanel.add(deck, Integer.valueOf(100));
+		
+		//Create user card panel
+		JPanel cardPanel = new JPanel();
+		tablePanel.add(cardPanel, Integer.valueOf(501));
+		cardPanel.setSize(400, 100);
+//		cardPanel.setLocation();
+		
+		//Create random cards(user)
+		JLabel randomCard; 
+		
+		
+		
+		
+		//Add user score
+		JLabel userScore = new JLabel("Score:");
+		userScore.setSize(150, 75);
+		userScore.setLocation(600, 350);
+		userScore.setForeground(Color.WHITE);
+		tablePanel.add(userScore);
+		tablePanel.setLayer(userScore, 200);
+		
+		//add user score number
+		userScoreNumber = new JLabel("0");
+		tablePanel.add(userScoreNumber);
+		userScoreNumber.setSize(150, 75);
+		userScoreNumber.setLocation(645, 350);
+		userScoreNumber.setForeground(Color.WHITE);
+		tablePanel.setLayer(userScoreNumber, 200);
+		
+		//Add computer score
+		JLabel computerScore = new JLabel("Score:");
+		tablePanel.add(computerScore);
+		computerScore.setSize(150, 50);
+		computerScore.setLocation(600, 0);
+		computerScore.setForeground(Color.WHITE);
+		tablePanel.setLayer(computerScore, 201);
+		
+		//Add computer score number
+		computerScoreNumber = new JLabel("0");
+		tablePanel.add(computerScoreNumber);
+		computerScoreNumber.setSize(150, 50);
+		computerScoreNumber.setLocation(645, 0);
+		computerScoreNumber.setForeground(Color.WHITE);
+		tablePanel.setLayer(computerScoreNumber, 201);
+		
+		//Create button Panel
+		JPanel buttonPanel = new JPanel();
+		tablePanel.add(buttonPanel, Integer.valueOf(500));
+		buttonPanel.setSize(130, 40);
+		buttonPanel.setLocation(300, 355);
+		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.LINE_AXIS));
+		tablePanel.setLayer(buttonPanel, 405);
+		
+		//Add card button
+		JButton addCard = new JButton("Hit");
+		addCard.setPreferredSize(new Dimension(50, 25));
+		addCard.addActionListener(new HitButtonHandler());
+		addCard.setLocation(100, 0);
+		buttonPanel.add(addCard);
+		buttonPanel.add(Box.createRigidArea(new Dimension(5,0)));
+		
+		//Add no card button
+		JButton noCard = new JButton("Stand");
+		noCard.setPreferredSize(new Dimension(50, 25));
+		addCard.setLocation(100, 25); 
+		buttonPanel.add(noCard);
+		buttonPanel.add(Box.createRigidArea(new Dimension(5,0)));
+		
+		
+		//Card random generator(user)
+	
 		
 		
 		// Add the panel to the frame
@@ -205,6 +269,33 @@ public class FinalProject {
      * Subclasses that handle events (button clicks, mouse clicks and moves,
      * key presses, timer expirations)
      */
+	
+	private static void randomCard() {
+		Random randomGenerator = new Random();
+		int randomCard = randomGenerator.nextInt(53); 
+		cards [randomCard].setVisible(true);
+		
+			
+		}
+	
+	
+	
+	
+	private static class HitButtonHandler implements ActionListener {
+		public void actionPerformed (ActionEvent event) {
+			
+		}
+	}
+	
+	
+	
+	/**Handles clicks on new game button */
+    private static class NewGameHandler implements ActionListener {
+        public void actionPerformed (ActionEvent event) {
+            // Set the score to 0
+        	//scoreLabel.setText("0");
+        }
+    }
 	
 	   /** Handles clicks on quit button */
     private static class QuitButtonListener implements ActionListener {
